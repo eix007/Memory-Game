ab1226523be6bb35642795b31f0b1d0df80dd1a4
eix007 <anas.alabtah@gmail.com>
2016-11-15 02:48:50 -0800

    add score counter to the game

--- game.js
+++ game.js
@@ -7,7 +7,7 @@
 //-onclick function Store images sources and id's in Array to compare them
 //- Set conditions if the compare inside the array is making match keep the tow images flipped over if there is different flip it back 	to HYF image and chose again
 
-
+var Score = 1;
 //Define images array 
 var imgArray = ['images/img1.jpg', 'images/img2.jpg', 'images/img3.jpg', 'images/img4.jpg', 'images/img5.jpg', 'images/img6.jpg']
 
@@ -15,13 +15,14 @@ var imgArray = ['images/img1.jpg', 'images/img2.jpg', 'images/img3.jpg', 'images
 var imgDuble = imgArray.concat(imgArray);
 
 //Set action game variables  
-var firstClick = 0;
-var secondClick = -1;
+var cardsFlippedOver = 0;
+var lastCardPicked = -1;
 var flipArray = new Array();
 var timer = '';
 
-//Tagret the image's main div 
+
 var gameboard = document.getElementById("gameboard");
+var nav = document.getElementById("nav");
 
 gameStart();
 
@@ -35,29 +36,30 @@ function gameStart() {
 }
 
 //Compared images if there is a match keep the images flipped 
-function compare(a, n, s) {
-	if (firstClick < 2 && secondClick != n) {
-		flipArray[firstClick] = imgDuble[n];
-		flipArray[(firstClick + 2)] = s.id;
-		firstClick++;
-		s.src = imgDuble[n];
-		console.log(flipArray);
-		if (firstClick == 2) {
+function compare(imgSrc, num, imgTag) {
+	if (cardsFlippedOver < 2 && lastCardPicked != num) {
+		flipArray[cardsFlippedOver] = imgDuble[num];
+		flipArray[(cardsFlippedOver + 2)] = imgTag.id;
+		cardsFlippedOver++;
+		imgTag.src = imgDuble[num];
+		console.log(imgSrc);
+		if (cardsFlippedOver == 2) {
 			if (flipArray[0] == flipArray[1]) {
-				console.log('Match Found')
+				console.log('Match Found');
+				nav.innerHTML = 'Score : ' + Score++;
 				choseAgain();
 			} else {
 				timer = setInterval(hideImg, 2000);
 				console.log('Match Not Found');
 			}
 		}
-		secondClick = n;
+		lastCardPicked = num;
 	}
 }
 
 //Clear game action
 function choseAgain() {
-	firstClick = 0;
+	cardsFlippedOver = 0;
 	flipArray = [];
 	clearInterval(timer);
 }
